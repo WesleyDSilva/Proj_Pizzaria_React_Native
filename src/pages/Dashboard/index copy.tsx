5af407e8c2b76f79b9fa0f75db09450391979dbe
WesleyDSilva <wdias00@gmail.com>
2024-11-02 02:22:55 -0300

    Update Bottom Tabs

---
+++ src/pages/Dashboard/index copy.tsx
@@ -0,0 +1,64 @@
+import React, {useContext, useState} from "react";
+import {View, Text, Button, SafeAreaView,TouchableOpacity, TextInput, StyleSheet} from 'react-native'
+import { AuthContext } from "../../contexts/AuthContext";
+
+export default function Dashboard(): React.JSX.Element{
+    const {signOut} = useContext(AuthContext);
+    return(
+        <SafeAreaView style= {styles.container}>
+            <Text style={styles.title}>Novo Pedido</Text>
+
+            <TextInput placeholder="Número da Mesa" placeholderTextColor="#FFF" style={styles.input}
+                keyboardType="numeric"
+            />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Abrir mesa</Text>
+
+            </TouchableOpacity>
+               
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'center',
+        paddingVertical:15,
+        backgroundColor: '#F4F4F4',
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000',
+        marginBottom: 24,
+    },
+    input:{
+        width:'90%',
+        height:60,
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        paddingHorizontal:8,
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#000'
+    },
+    button:{
+        width: '90%',
+        height: 40,
+        backgroundColor: '#FFA831',
+        borderRadius: 4,
+        marginVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    buttonText:{
+        fontSize: 18,
+        color: '#fff',
+        fontWeight: 'bold'
+
+    }
+
+})
