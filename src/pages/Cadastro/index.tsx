3f1b257888787dc878176544ae32c7639b8c4d8b
WesleyDSilva <wdias00@gmail.com>
2025-03-23 23:11:26 -0300

    Adiciona cpf

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native"; // Importação do hook de navegação
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native'; // Importação do hook de navegação
 
-import { api_CEP } from "../../services/api_CEP";
-import { buscaEndereco } from "../../services/enderecoService";
-import { cadastrarUsuario } from "../../services/api_cadastro";
-
-
-
+import {api_CEP} from '../../services/api_CEP';
+import {buscaEndereco} from '../../services/enderecoService';
+//import { cadastrarUsuario } from "../../services/api_cadastro";
+import {cadastrarUsuario} from '../../services/api_cadastro';
 
 export default function Cadastro(): React.JSX.Element {
   const navigation = useNavigation(); // Uso do hook de navegação
   const [focusedField, setFocusedField] = useState<string | null>(null);
 
   const [nome, setNome] = useState('');
+  const [cpf, setCpf] = useState('');
   const [email, setEmail] = useState('');
   const [telefone, setTelefone] = useState('');
   const [cep, setCep] = useState('');
@@ -25,68 +32,72 @@ export default function Cadastro(): React.JSX.Element {
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
 
-  const handleCepChange = async (value: string) =>{
+  const handleCepChange = async (value: string) => {
     setCep(value);
-    if(value.length === 8){
-      try{
+    if (value.length === 8) {
+      try {
         const endereco = await buscaEndereco(value);
-        setLogradouro(endereco.logradouro)
-        setCidade(endereco.cidade)
-        setEstado(endereco.estado)
-      } catch (error:any){
+        setLogradouro(endereco.logradouro);
+        setCidade(endereco.cidade);
+        setEstado(endereco.estado);
+      } catch (error: any) {
         Alert.alert('Erro', error.message);
       }
     }
-  }
+  };
 
   async function handleCadastro() {
     if (senha !== confirmaSenha) {
       Alert.alert('As senhas não conferem');
       return;
-    } 
+    }
 
-      const data = {
-        nome,
-        logradouro,
-        cidade,
-        UF: estado,
-        cep,
-        complemento,
-        numero_casa: numero,
-        email,
-        telefone,
-        senha,
-      };
+    const data = {
+      nome,
+      cpf,
+      logradouro,
+      cidade,
+      UF: estado,
+      cep,
+      complemento,
+      numero_casa: numero,
+      email,
+      telefone,
+      senha,
+    };
 
-      try {
-        await cadastrarUsuario(data);
-        Alert.alert("Sucesso", "Usuário cadastrado com sucesso!");
-        // Resetar os campos do formulário, se necessário
-        setNome('');
-        setEmail('');
-        setTelefone('');
-        setCep('');
-        setEstado('');
-        setCidade('');
-        setLogradouro('');
-        setNumero('');
-        setComplemento('');
-        setSenha('');
-        setConfirmaSenha('');
-      } catch (error: any) {
-        Alert.alert("Erro", error.message);
-      }
+    try {
+      await cadastrarUsuario(data);
+      Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!');
+      // Resetar os campos do formulário, se necessário
+      setNome('');
+      setCpf('');
+      setEmail('');
+      setTelefone('');
+      setCep('');
+      setEstado('');
+      setCidade('');
+      setLogradouro('');
+      setNumero('');
+      setComplemento('');
+      setSenha('');
+      setConfirmaSenha('');
+    } catch (error: any) {
+      Alert.alert('Erro', error.message);
+    }
   }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-     {/* Botão no topo para voltar */}
-     <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Text style={styles.backText}>{"<"}</Text>
+      {/* Botão no topo para voltar */}
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}>
+        <Text style={styles.backText}>{'<'}</Text>
       </TouchableOpacity>
-    
-          <Text style={styles.title}>Cadastro</Text>
-      
+
+      <Text style={styles.title}>Cadastro</Text>
+
       <TextInput
         placeholder="Nome Completo"
         style={[styles.input, focusedField === 'nome' && styles.inputFocused]}
@@ -96,6 +107,18 @@ export default function Cadastro(): React.JSX.Element {
         value={nome}
         onChangeText={setNome}
       />
+
+      <TextInput
+        placeholder="CPF"
+        style={[styles.input, focusedField === 'cpf' && styles.inputFocused]}
+        onFocus={() => setFocusedField('cpf')}
+        onBlur={() => setFocusedField(null)}
+        placeholderTextColor="#474747"
+        value={cpf}
+        onChangeText={setCpf}
+        keyboardType="numeric"
+      />
+
       <TextInput
         placeholder="E-mail"
         style={[styles.input, focusedField === 'email' && styles.inputFocused]}
@@ -108,7 +131,10 @@ export default function Cadastro(): React.JSX.Element {
 
       <TextInput
         placeholder="Telefone Celular"
-        style={[styles.input, focusedField === 'telefone' && styles.inputFocused]}
+        style={[
+          styles.input,
+          focusedField === 'telefone' && styles.inputFocused,
+        ]}
         onFocus={() => setFocusedField('telefone')}
         onBlur={() => setFocusedField(null)}
         placeholderTextColor="#474747"
@@ -130,7 +156,11 @@ export default function Cadastro(): React.JSX.Element {
       <View style={styles.row}>
         <TextInput
           placeholder="Cidade"
-          style={[styles.input, focusedField === 'cidade' && styles.inputFocused, {width:'70%'}]}
+          style={[
+            styles.input,
+            focusedField === 'cidade' && styles.inputFocused,
+            {width: '70%'},
+          ]}
           onFocus={() => setFocusedField('cidade')}
           onBlur={() => setFocusedField(null)}
           placeholderTextColor="#474747"
@@ -140,7 +170,11 @@ export default function Cadastro(): React.JSX.Element {
 
         <TextInput
           placeholder="UF"
-          style={[styles.input, focusedField === 'estado' && styles.inputFocused, {width:'20%'}]}
+          style={[
+            styles.input,
+            focusedField === 'estado' && styles.inputFocused,
+            {width: '20%'},
+          ]}
           onFocus={() => setFocusedField('estado')}
           onBlur={() => setFocusedField(null)}
           placeholderTextColor="#474747"
@@ -149,11 +183,12 @@ export default function Cadastro(): React.JSX.Element {
         />
       </View>
 
-
-
       <TextInput
         placeholder="Logradouro"
-        style={[styles.input, focusedField === 'logradouro' && styles.inputFocused]}
+        style={[
+          styles.input,
+          focusedField === 'logradouro' && styles.inputFocused,
+        ]}
         onFocus={() => setFocusedField('logradouro')}
         onBlur={() => setFocusedField(null)}
         placeholderTextColor="#474747"
@@ -164,7 +199,10 @@ export default function Cadastro(): React.JSX.Element {
       <View style={styles.row}>
         <TextInput
           placeholder="Número"
-          style={[styles.inputHalf, focusedField === 'numero' && styles.inputFocused]}
+          style={[
+            styles.inputHalf,
+            focusedField === 'numero' && styles.inputFocused,
+          ]}
           onFocus={() => setFocusedField('numero')}
           onBlur={() => setFocusedField(null)}
           placeholderTextColor="#474747"
@@ -172,10 +210,13 @@ export default function Cadastro(): React.JSX.Element {
           onChangeText={setNumero}
           keyboardType="numeric"
         />
-        
+
         <TextInput
           placeholder="Complemento"
-          style={[styles.inputHalf, focusedField === 'complemento' && styles.inputFocused]}
+          style={[
+            styles.inputHalf,
+            focusedField === 'complemento' && styles.inputFocused,
+          ]}
           onFocus={() => setFocusedField('complemento')}
           onBlur={() => setFocusedField(null)}
           placeholderTextColor="#474747"
@@ -197,7 +238,10 @@ export default function Cadastro(): React.JSX.Element {
 
       <TextInput
         placeholder="Confirme Senha"
-        style={[styles.input, focusedField === 'confirmaSenha' && styles.inputFocused]}
+        style={[
+          styles.input,
+          focusedField === 'confirmaSenha' && styles.inputFocused,
+        ]}
         onFocus={() => setFocusedField('confirmaSenha')}
         onBlur={() => setFocusedField(null)}
         placeholderTextColor="#474747"
@@ -208,7 +252,6 @@ export default function Cadastro(): React.JSX.Element {
       <TouchableOpacity style={styles.button} onPress={handleCadastro}>
         <Text style={styles.buttonText}>Cadastrar</Text>
       </TouchableOpacity>
-      
     </ScrollView>
   );
 }
@@ -222,19 +265,19 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
   backButton: {
-    position: "absolute",
+    position: 'absolute',
     top: 20,
     left: 10,
     zIndex: 1,
   },
   backText: {
     fontSize: 24,
-    fontWeight: "bold",
-    color: "#000",
+    fontWeight: 'bold',
+    color: '#000',
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
     marginTop: 50,
   },
@@ -265,7 +308,8 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: '#000',
-  },  row: {
+  },
+  row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '95%',
