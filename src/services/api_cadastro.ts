e39a4b368499aa223ca8ed33cdb4d4e16afaee95
WesleyDSilva <wdias00@gmail.com>
2025-03-23 23:11:51 -0300

    Update api_cadastro.ts

--- src/services/api_cadastro.ts
+++ src/services/api_cadastro.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "https://devweb3.ok.etc.br"; // Substitua pelo URL base da sua API
+const API_URL = 'https://devweb3.ok.etc.br'; // Substitua pelo URL base da sua API
 
 interface CadastroData {
   nome: string;
@@ -13,19 +13,26 @@ interface CadastroData {
   email: string;
   telefone: string;
   senha: string;
+  cpf: string;
 }
 
 export async function cadastrarUsuario(data: CadastroData): Promise<void> {
   try {
-    const response = await axios.post(`${API_URL}/api/api_create.php`, data);
+    //const response = await axios.post(`${API_URL}/api/api_create.php`, data);
+
+    const response = await axios.post(
+      `${API_URL}/api/api_create_hash.php`,
+      data,
+    );
+    //const response = await axios.post(`${API_URL}/api/api_recebimento_cadastro.php`,data);
     if (response.status === 200 || response.status === 201) {
       return response.data;
     } else {
-      throw new Error(response.data.message || "Erro ao cadastrar usuário");
+      throw new Error(response.data.message || 'Erro ao cadastrar usuário');
     }
   } catch (error: any) {
     throw new Error(
-      error.response?.data?.message || "Erro ao conectar com a API"
+      error.response?.data?.message || 'Erro ao conectar com a API',
     );
   }
 }
