6af0c2dc7b88fe10776121bc769728b8f599b7c2
WesleyDSilva <wdias00@gmail.com>
2025-03-23 23:11:41 -0300

    Update authroutes.tsx

--- src/routes/authroutes.tsx
+++ src/routes/authroutes.tsx
@@ -1,24 +1,23 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Cadastro from '../pages/Cadastro';
 
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Cadastro from "../pages/Cadastro";
-
-import SignIn from "../pages/SignIn";
-import Cadastro from "../pages/Cadastro"; // Certifique-se de que esta importação esteja correta
-
+import SignIn from '../pages/SignIn';
+//import Cadastro from "../pages/Cadastro"; // Certifique-se de que esta importação esteja correta
 
 const Stack = createNativeStackNavigator();
 
-
-
-function AuthRoutes(){
-    return(
-        <Stack.Navigator initialRouteName="SignIn">
-            <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
-            <Stack.Screen name="Cadastro" component={Cadastro} />
-            
-        </Stack.Navigator>
-    );
+function AuthRoutes() {
+  return (
+    <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Cadastro" component={Cadastro} />
+    </Stack.Navigator>
+  );
 }
 
 export default AuthRoutes;
