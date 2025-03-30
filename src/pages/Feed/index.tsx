3f41355dc47999b7dd2172adaaaed2c875fd2056
WesleyDSilva <wdias00@gmail.com>
2025-03-29 23:58:54 -0300

    Update index.tsx

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,27 +1,33 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import Feed_Pizza from './Feed_Pizza';
 
-const Feed =  (): React.JSX.Element =>{
+const Feed = (): React.JSX.Element => {
   return (
     <View style={styles.container}>
-      <Feed_Pizza/>
-      
+      <Image source={require('../../assets/Welcome_text.jpg')} />
+      <Feed_Pizza />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start', // Alinha a imagem à esquerda
     justifyContent: 'center',
+    paddingLeft: 10, // Ajusta o espaço à esquerda, caso necessário
   },
-  title:{
-    fontSize:22,
-    fontWeight:'bold'
-  }
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%', // Ajusta a largura para 100% da tela
+    height: 200, // Altura fixa para a imagem
+    resizeMode: 'cover', // Ajuste o modo de exibição da imagem
+    marginBottom: 20, // Espaço abaixo da imagem
+  },
 });
 
 export default Feed;
